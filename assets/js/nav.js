4105f0f8fc3c5af8d132f273134bba6d1649aeff
yeomanse <yeomanse@vt.edu>
2013-09-22 07:04:33 -0400

    ok?

---
+++ assets/js/nav.js
@@ -0,0 +1,10 @@
+﻿function activateListType(event){
+	lis[x] = document.getElementsByName("navli");
+	for each (x in lis){
+		if(lis[x].className == "active list-type"){
+			alert(lis[x]);
+			lis[x].className = "list-type";
+		}
+	}
+	event.className = "active list-type";
+}
